8f1f180d9af1031c347c89352a146451eb0aecbb
Mauricio Flores Pradel <mauricio.flores.enp5.063@gmail.com>
2024-02-23 14:55:14 -0600

    Ejercicio 4

--- app/src/main/java/com/mexiti/listacomidaor/MainActivity.kt
+++ app/src/main/java/com/mexiti/listacomidaor/MainActivity.kt
@@ -1,22 +1,29 @@
 package com.mexiti.listacomidaor
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Menu
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -52,20 +59,30 @@ fun MenuApp(){
     MenuCardList(platilloList = DataSource().LoadPlatillos())
 }
 
+
 @Composable
-fun MenuCardList( platilloList: List<Platillo>,modifier: Modifier= Modifier   ){
+fun MenuCardList( platilloList: List<Platillo>,modifier: Modifier= Modifier   ) {
 
-    LazyColumn(modifier = modifier){
-        items( platilloList ){
-            platillo -> MenuCard(
-            platillo = platillo,
-                modifier = modifier.padding(10.dp)
+    Scaffold(
+        topBar = {
+            MenuTopAppBar()
+        }
+    ) {
+        it ->
 
-            )
+
+        LazyColumn(contentPadding = it) {
+            items(platilloList) { platillo ->
+                MenuCard(
+                    platillo = platillo,
+                    modifier = modifier.padding(10.dp)
+
+                )
+            }
+
         }
 
     }
-
 }
 
 @Composable
@@ -85,7 +102,7 @@ fun MenuCard(platillo:Platillo, modifier: Modifier = Modifier ){
             Text(
                 text = LocalContext.current.getString(platillo.stringResourceId),
                 modifier = modifier.padding(22.dp),
-                style = MaterialTheme.typography.headlineMedium
+                style = MaterialTheme.typography.displayMedium
             )
             
         }
@@ -93,10 +110,38 @@ fun MenuCard(platillo:Platillo, modifier: Modifier = Modifier ){
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuTopAppBar(modifier: Modifier = Modifier){
+CenterAlignedTopAppBar(
+    title = {
+        Row (verticalAlignment = Alignment.CenterVertically){
+            Image(
+                painter = painterResource(id = R.drawable.logo_platillo),
+                contentDescription = null,
+                modifier = modifier
+                    .padding(8.dp)
+                    .size(100.dp)
+
+                )
+            Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+
+        }
+    },
+    modifier = modifier
+)
+}
+
 @Composable
 @Preview(showBackground = true)
 fun ShowMenuCard(){
-    MenuCardList(platilloList = DataSource().LoadPlatillos()  )
+    ListaComidaOrTheme(darkTheme = false) {
+        MenuCardList(platilloList = DataSource().LoadPlatillos()  )
+    }
+
 }
 
 
--- app/src/main/java/com/mexiti/listacomidaor/ui/theme/Type.kt
+++ app/src/main/java/com/mexiti/listacomidaor/ui/theme/Type.kt
@@ -1,20 +1,57 @@
 package com.mexiti.listacomidaor.ui.theme
 
+import android.app.VoiceInteractor.Prompt
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mexiti.listacomidaor.R
 
 // Set of Material typography styles to start with
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold)
+)
+val AbrilFatFace = FontFamily(
+    Font(R.font.abrilfatface_regular)
+)
+val Prompt = FontFamily(
+    Font(R.font.prompt_regular)
+)
+val Oswald = FontFamily(
+    Font(R.font.oswald_bold)
+)
+val Roboto = FontFamily(
+    Font(R.font.robotocondensed_regular)
+)
+
+
+
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = Oswald,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 36.sp,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Prompt,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Prompt,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -31,4 +68,5 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+
 )
